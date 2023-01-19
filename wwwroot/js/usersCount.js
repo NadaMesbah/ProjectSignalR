d68ec25f656c2b22828f7116a65a6d5926a8fff0
nada <nada.mesbah@usmba.ac.ma>
2023-01-19 23:31:48 +0100

    Connecting to Hub from Client

---
+++ wwwroot/js/usersCount.js
@@ -0,0 +1,28 @@
+﻿const { signalR } = require("./signalr/dist/browser/signalr");
+
+//create connection for our SignalR Hub
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka recieve notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient()
+{
+    connectionUserCount.send("NewWindowLoaded")
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+}
+
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
